ef3d2a0e1b221234fbbecb0dff4837023a03c6c6
renatorqg <renatorqg@gmail.com>
2019-09-14 10:56:22 -0300

    create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <ratio.h>
+
+using namespace std;
+
+int main()
+{
+  cout << "Hello ratio" << endl;
+	Ratio x(22,7), y(-3,8), z;
+	z = x; // using the assignment operator 
+	z.print(); cout << endl;
+	x = y*z; // using the multiplication operator
+	x.print(); cout << endl;
+}
